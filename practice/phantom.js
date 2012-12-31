a253b6dd310eb01f21b9bc10df1931961eec77a4
cczopy <xieren58@gmail.com>
2012-12-31 16:08:46 +0800

    update phantom.js

--- practice/phantom.js
+++ practice/phantom.js
@@ -1,11 +1,15 @@
 var page = require('webpage').create();
 
-var url = 'http://www.17qingsong.com/news/8JP4335P00964JJM';
+var url = 'http://www.17qingsong.com/news/8JTND98F00964JJM';
 
-page.viewportSize = { width: 1170, height: 6608 };
+page.viewportSize = {width: 1170, height: 7824};
 
 page.open(url, function (status) {
-    page.render('17.png');
+    // var height = page.evaluate(function() { return document.body.clientHeight; });
+    // var width = page.evaluate(function() { return document.body.clientWidth; });
+    // console.log(height);
+    // page.viewportSize = {width: 1170, height: height};
+    page.render('17.jpg');
     // page.render('17.pdf');
     phantom.exit();
 });
