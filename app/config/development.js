1960af2472c8865dde796764ecf42fd966556ead
cczopy <xieren58@gmail.com>
2013-01-25 15:47:05 +0800

    update interval

--- app/config/development.js
+++ app/config/development.js
@@ -2,7 +2,7 @@
 module.exports = {
   Config: {
     port: 3000,
-    interval: 1000 * 60 * 10,
+    interval: 1000 * 60 * 30,
     staticMaxAge: 3600000 * 24 * 15,
     limit: 15
   }
--- app/config/production.js
+++ app/config/production.js
@@ -2,7 +2,7 @@
 module.exports = {
   Config: {
     port: process.env.VCAP_APP_PORT || process.env.PORT || 3000,
-    interval: 1000 * 60 * 10,
+    interval: 1000 * 60 * 30,
     staticMaxAge: 3600000 * 24 * 15,
     limit: 15
   }
